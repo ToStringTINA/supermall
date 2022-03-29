776b6cac63462147eb09127efe68c05711d7bfb2
tao_0416 <961096969@qq.com>
2022-03-29 19:42:39 +0800

    完成首页部分功能

---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="recommend">
+    <div v-for="item in recommend" class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "RecommendView",
+  props: {
+    recommend: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .recommend {
+    display: flex;
+    margin-top: 10px;
+    font-size: 14px;
+    padding-bottom: 30px;
+    border-bottom: 8px solid #eee;
+  }
+  .recommend-item {
+    flex: 1;
+    text-align: center;
+  }
+  .recommend-item img  {
+    width: 70px;
+    height: 70px;
+    margin-bottom: 10px;
+  }
+</style>
